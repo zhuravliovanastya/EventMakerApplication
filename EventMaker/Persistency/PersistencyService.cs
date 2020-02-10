e0a8d6b6e285d01f5b72f93f02928d350ffd2f77
zhuravliovanastya <55129360+zhuravliovanastya@users.noreply.github.com>
2020-02-10 10:55:50 +0100

    awdwed

--- EventMaker/Persistency/PersistencyService.cs
+++ EventMaker/Persistency/PersistencyService.cs
@@ -8,6 +8,13 @@ namespace EventMaker.Persistency
 {
     class PersistencyService
     {
+        public static async void SaveEventsAsJsonAsync(ObservableCollection<Event> events) {}
+
+        public static async Task<List<Event>> LoadEventsFromJsonAsync() { }
+
+        public static async void SerializeEventsFileAsync(string eventsString, string fileName) { }
+
+        public static async Task<string> DeSerializeEventsFileAsync(String fileName) { }
 
     }
 
